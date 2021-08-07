df99cf1958fd25631284118033cdbabc45f92356
Kevin Albrecht <onlyafly@gmail.com>
2021-08-07 21:43:08 +0200

    Working!

--- main.ts
+++ main.ts
@@ -1,144 +1,138 @@
-import { replaceHeaderNumbering } from 'numbering';
-import { App, MarkdownView, Modal, Plugin } from 'obsidian';
+import { replaceHeaderNumbering } from 'numbering'
+import { App, MarkdownView, Modal, Plugin } from 'obsidian'
 
 /* Unused feature: settings
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  mySetting: 'default'
 }
 */
 
+class JobDoneModal extends Modal {
+  message: string
+
+  constructor (app: App, message: string) {
+    super(app)
+    this.message = message
+  }
+
+  onOpen () {
+    const { contentEl, titleEl } = this
+    contentEl.setText(this.message)
+    titleEl.setText('Header Numbering Plugin')
+  }
+
+  onClose () {
+    const { contentEl, titleEl } = this
+    contentEl.empty()
+    titleEl.empty()
+  }
+}
+
+function showJobDoneMessage (app: App, message: string) {
+  const leaf = app.workspace.activeLeaf
+  if (leaf) {
+    new JobDoneModal(app, message).open()
+  }
+}
+
 export default class MyPlugin extends Plugin {
-	/* Unused feature: settings
-	settings: MyPluginSettings;
-	*/
+  /* Unused feature: settings
+  settings: MyPluginSettings;
+  */
 
-	async onload() {
-		console.log('loading HN plugin, v0.0.1.4');
+  async onload () {
+    // eslint-disable-next-line no-console
+    console.log('loading HN plugin, v0.0.1.11')
 
-		/* Unused feature: settings
-		await this.loadSettings();
-		*/
+    /* Unused feature: settings
+    await this.loadSettings();
+    */
 
-		/* Unused feature: ribbon icon
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
-		*/
+    /* Unused feature: ribbon icon
+    this.addRibbonIcon('dice', 'Sample Plugin', () => {
+      new Notice('This is a notice!');
+    });
+    */
 
-		/* Unused feature: status text
-		this.addStatusBarItem().setText('Status Bar Text');
-		*/
+    /* Unused feature: status text
+    this.addStatusBarItem().setText('Status Bar Text');
+    */
 
-		this.addCommand({
-			id: 'number-headings',
-			name: 'Number Headings',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (activeView && activeView.file) {
+    this.addCommand({
+      id: 'number-headings',
+      name: 'Number Headings',
+      // callback: () => {
+      //   console.log('Simple Callback');
+      // },
+      checkCallback: (checking: boolean) => {
+        const activeView = this.app.workspace.getActiveViewOfType(MarkdownView)
+        if (activeView && activeView.file) {
+          if (!checking) {
+            const editor = activeView.editor
+            const data = this.app.metadataCache.getFileCache(activeView.file) || {}
+            replaceHeaderNumbering(data, editor)
 
-					if (!checking) {
-						const editor = activeView.editor;
-						const cursor = editor.getCursor();
-						const data = this.app.metadataCache.getFileCache(activeView.file) || {};
-						replaceHeaderNumbering(data, cursor, editor);
+            showJobDoneMessage(this.app, 'Successfully updated all header numbering in the document.')
+          }
 
-						showMessage(this.app)
-					}
+          return true
+        }
 
-					return true;
-				}
+        return false // This command is not available if the view is not selected
+      }
+    })
 
-				return false; // This command is not available if the view is not selected
-			}
-		});
+    /* Unused feature: settings
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+    */
+  }
 
-		/* Unused feature: settings
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-		*/
+  onunload () {
+    // DELETE console.log('unloading plugin');
+  }
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
+  /* Unused feature: settings
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			//DELETE console.log('click (from header numbering plugin)', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		// DELETE console.log('unloading plugin');
-	}
-
-	/* Unused feature: settings
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-	*/
-}
-
-function showMessage(app: App) {
-	let leaf = app.workspace.activeLeaf;
-	if (leaf) {
-		new SampleModal(app).open();
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let { contentEl } = this;
-		contentEl.empty();
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+  */
 }
 
 /* Unused feature: settings
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		let { containerEl } = this;
+  display(): void {
+    let { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
+    containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc('It\'s a secret')
+      .addText(text => text
+        .setPlaceholder('Enter your secret')
+        .setValue('')
+        .onChange(async (value) => {
+          console.log('Secret: ' + value);
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        }));
+  }
 }
 */
--- numbering.ts
+++ numbering.ts
@@ -1,84 +1,72 @@
-import { CachedMetadata, Editor, EditorPosition, HeadingCache } from 'obsidian';
+import { CachedMetadata, Editor } from 'obsidian'
 
-const getCurrentHeaderDepth = (
-  headings: HeadingCache[],
-  cursor: EditorPosition
-): number => {
-  const previousHeadings = headings.filter(
-    (heading) => heading.position.end.line < cursor.line
-  );
+function makeNumberingString (numberingStack: number[]): string {
+  let builder = ''
 
-  if (!previousHeadings.length) {
-    return 0;
+  for (let i = 0; i < numberingStack.length; i++) {
+    if (i !== 0) {
+      builder += '.'
+    }
+    builder += numberingStack[i].toString()
   }
 
-  return previousHeadings[previousHeadings.length - 1].level;
-};
+  const headerHashes = '#'.repeat(numberingStack.length)
 
-const getSubsequentHeadings = (
-  headings: HeadingCache[],
-  cursor: EditorPosition
-): HeadingCache[] => {
-  return headings.filter((heading) => heading.position.end.line > cursor.line);
-};
+  return headerHashes + ' ' + builder
+}
 
 export const replaceHeaderNumbering = (
   { headings = [] }: CachedMetadata,
-  cursor: EditorPosition,
-  editor: Editor,
-): string | undefined => {
-  console.log('replaceHeaderNumbering 100')
+  editor: Editor
+) => {
+  let previousLevel = 1
+  const numberingStack: number[] = [0]
 
-  const currentDepth = getCurrentHeaderDepth(headings, cursor);
-  const subsequentHeadings = getSubsequentHeadings(headings, cursor);
-  const includedHeadings: HeadingCache[] = [];
+  for (const heading of headings) {
+    // Update the numbering stack based on the level and previous level
 
-  console.log('replaceHeaderNumbering 200')
+    const level = heading.level
 
-  for (const heading of subsequentHeadings) {
-    console.log('replaceHeaderNumbering -- for 300')
-    if (heading.level <= currentDepth) {
-      break;
+    if (level === previousLevel) {
+      const x = numberingStack.pop()
+      numberingStack.push(x + 1)
+    } else if (level < previousLevel) {
+      for (let i = previousLevel; i > level; i--) {
+        numberingStack.pop()
+      }
+      const x = numberingStack.pop()
+      numberingStack.push(x + 1)
+    } else if (level > previousLevel) {
+      for (let i = previousLevel; i < level; i++) {
+        numberingStack.push(1)
+      }
     }
 
-    if (
-      heading.level >= 1 &&
-      heading.level <= 6
-    ) {
-      includedHeadings.push(heading);
+    // Set the previous level to this level for the next iteration
+    previousLevel = level
+
+    const regex = /^#+ ([0-9.]+)?( )*/g
+    const headerLineString = editor.getLine(heading.position.start.line)
+    const matches = headerLineString.match(regex)
+
+    if (matches.length !== 1) {
+      // eslint-disable-next-line no-console
+      console.log("Unexpected header format: '" + headerLineString + "'")
+      continue
     }
-    console.log('replaceHeaderNumbering -- for 310')
+
+    const match = matches[0]
+
+    const from = {
+      line: heading.position.start.line,
+      ch: 0
+    }
+    const to = {
+      line: heading.position.start.line,
+      ch: match.length
+    }
+
+    const numberingString = makeNumberingString(numberingStack)
+    editor.replaceRange(numberingString + ' ', from, to)
   }
-
-  console.log('replaceHeaderNumbering -- 400')
-
-  if (!includedHeadings.length) {
-    // No headings found
-    return;
-  }
-
-  console.log('replaceHeaderNumbering -- for 500')
-
-  const listStyle = "number"
-  const firstHeadingDepth = includedHeadings[0].level;
-  const links = includedHeadings.map((heading) => {
-    const itemIndication = (listStyle === "number" && "1.") || "-";
-    const indent = new Array(Math.max(0, heading.level - firstHeadingDepth))
-      .fill("\t")
-      .join("");
-
-    return `${indent}${itemIndication} [[#${heading.heading}|${heading.heading}]]`;
-  });
-
-
-
-  /* DELETE ME
-  return endent`
-    ${settings.title ? `${settings.title}\n` : ""}
-    ${`${links.join("\n")}\n`}
-  `;
-  */
-
-  editor.replaceRange("hola", cursor);
-  editor.replaceRange(links.join("\n"), cursor);
-};
+}
